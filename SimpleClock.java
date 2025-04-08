4213c7fa5cc5b1c30e4d595356eee3553a5c31e4
Isaac Mills-Owoo <poppymills2@gmail.com>
2025-04-08 01:23:31 -0400

    completed buttons

--- SimpleClock.java
+++ SimpleClock.java
@@ -2,8 +2,11 @@
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 
 public class SimpleClock extends JFrame {
@@ -16,16 +19,20 @@ public class SimpleClock extends JFrame {
         JLabel timeLabel;
         JLabel dayLabel;
         JLabel dateLabel;
+        JButton militaryTimeButton;
+        JButton localGMTButton;
         String time;
         String day;
         String date;
 
+
         SimpleClock() {
             this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             this.setTitle("Digital Clock");
-            this.setLayout(new FlowLayout(FlowLayout.CENTER, 10,10));
-            this.setSize(400, 250);
+            this.setLayout(new FlowLayout(FlowLayout.CENTER, 20,10));
+            this.setSize(420, 300);
             this.setResizable(false);
+
     
             timeFormat = new SimpleDateFormat("hh:mm:ss a");
             dayFormat=new SimpleDateFormat("EEEE");
@@ -40,12 +47,24 @@ public class SimpleClock extends JFrame {
             dayLabel.setLayout(new FlowLayout(FlowLayout.CENTER));
     
             dateLabel=new JLabel();
-            dateLabel.setFont(new Font("Ink Free",Font.BOLD,30));
-    
+            dateLabel.setFont(new Font("Ink Free",Font.BOLD,36));
+            dateLabel.setLayout(new FlowLayout(FlowLayout.CENTER,0,40));
+
+            militaryTimeButton = new JButton("24 hour");
+            militaryTimeButton.addActionListener(setMilitaryTimeButton());
+            militaryTimeButton.setLayout(new FlowLayout(FlowLayout.LEADING));
+
+
+            localGMTButton = new JButton("Local/ GMT");
+            localGMTButton.addActionListener(setMilitaryTimeButton());
+            localGMTButton.setLayout(new FlowLayout(FlowLayout.TRAILING));
     
             this.add(timeLabel);
             this.add(dayLabel);
             this.add(dateLabel);
+
+            this.add(militaryTimeButton);
+            this.add(localGMTButton);
             this.setVisible(true);
     
             setTimer();
@@ -73,6 +92,15 @@ public class SimpleClock extends JFrame {
             Thread thread = new Thread(task);
             thread.start();
         }
+
+        public ActionListener setMilitaryTimeButton(){
+            return e -> {
+                SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm:ss"); // 24-hour format
+                time = timeFormat.format(GregorianCalendar.getInstance().getTime());
+                timeLabel.setText(time);
+            };
+            }
+
         public static void main(String[] args) {
             new SimpleClock();
         }
